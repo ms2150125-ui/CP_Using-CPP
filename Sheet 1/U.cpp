a8b895f0e6db66504b01ea9e1b3a2746cfa0bdf3
Md. Abu Sayeed <ms2150125@gmail.com>
2026-09-11 00:57:56 +0600

    Add number type determination for input string
    The program reads a number as a string and determines if it is an integer or a float. It outputs the appropriate type along with the formatted number.

--- Sheet 1/U.cpp
+++ Sheet 1/U.cpp
@@ -1,1 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+	string number;
+	cin >> number;
+
+	size_t decimalPoint = number.find('.');
+	string integerPart = number.substr(0, decimalPoint);
+
+	if (decimalPoint == string::npos ||
+		number.substr(decimalPoint + 1).find_first_not_of('0') == string::npos)
+	{
+		cout << "int " << integerPart;
+	}
+	else
+	{
+		cout << "float " << integerPart << ' '
+				 << "0." << number.substr(decimalPoint + 1);
+	}
+
+	return 0;
+}
