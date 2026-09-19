08950d7415e47e031e25dbab545405814aa113d8
Md. Abu Sayeed <ms2150125@gmail.com>
2026-09-19 19:51:35 +0600

    Add files via upload

---
+++ Sheet -2/C_Even_Odd_Positive_and_Negative.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int a[n];
+
+    int even = 0, odd = 0, positive = 0, negative = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+
+        if(a[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+
+        if(a[i] > 0)
+            positive++;
+        else if(a[i] < 0)
+            negative++;
+    }
+
+    cout << "Even: " << even << endl;
+    cout << "Odd: " << odd << endl;
+    cout << "Positive: " << positive << endl;
+    cout << "Negative: " << negative << endl;
+
+    return 0;
+}
